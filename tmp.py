afd6df0e0a68ff91dcf627068516294859a3b850
Oskar Bosgraaf <oskar.bosgraaf@gmail.com>
2026-01-29 20:06:33 +0100

    final clustering coefficient plots

--- tmp.py
+++ tmp.py
@@ -15,7 +15,7 @@ def main():
     weight_range = (-0.04, 0.2)  # weight range changed from (-1,1.0) to (-0.1, 0.2) - individuals within the network tend to agree with their neighbours 
     avg_deg = 20
     seed = 1
-    n_steps = 500
+    n_steps = 250
 
     # Erdos_Renyi
     edge_prob = avg_deg / (N - 1) 
@@ -28,9 +28,9 @@ def main():
     rewire_prob = 0.1  # rewiring probability
 
     # Erdos-Renyi
-    # adj_mat = network_generation.generate_erdos_renyi_sparse_adjacency_matrix(
-    # 	num_nodes=N, edge_prob=edge_prob, weight_range=weight_range
-    # )
+    adj_mat = network_generation.generate_erdos_renyi_sparse_adjacency_matrix(
+        num_nodes=N, edge_prob=edge_prob, weight_range=weight_range
+    )
 
     # Scale-Free
     # adj_mat = network_generation.generate_scale_free_sparse_adjacency_matrix_jax(
@@ -38,9 +38,9 @@ def main():
     # )
 
     # Small-World
-    adj_mat = network_generation.generate_small_world_sparse_adjacency_matrix_jax(
-        num_nodes=N, k=k_neighbours, p=rewire_prob, weight_range=weight_range
-    )
+    # adj_mat = network_generation.generate_small_world_sparse_adjacency_matrix_jax(
+    #     num_nodes=N, k=k_neighbours, p=rewire_prob, weight_range=weight_range
+    # )
 
     # Initialisation
     ones_init = jnp.negative(jnp.ones(shape=(len(adj_mat),)).astype(int))
@@ -51,21 +51,22 @@ def main():
     def external_field(t, node_idx): return 10 * ((node_idx[0] / len(adj_mat)) < l) * (jnp.exp(-((t - alpha) / gamma)**2))
 
     # lambda - fraction of people being exposed to the dynamic field
-    l = 0.4  # % get the field
+    beta = 1.5  # temperature
+    l = 0.6  # % get the field
+    plotname = "vulnerable"
 
-    µ = 0.9  # memory coefficient
-    beta = 1.0  # temperature
+    µ = 1  # memory coefficient
 
-    interval = 5
+    interval = 2
     trials = 100
-    epochs = 2
+    epochs = 10
 
     x_ticks = list(range(0, n_steps + 1, interval))
     nbs, _ = adj_mat.precompute_neighbors_and_weights()
     nbs = [np.asarray(nb[nb > -1]) for nb in nbs]
     coeffs_over_time = []
     mags = []
-    fig, axs = plt.subplots(nrows=2, ncols=1, sharex=True, figsize=(15, 9))
+    fig, axs = plt.subplots(nrows=2, ncols=1, sharex=True, figsize=(5, 6))
     for i in tqdm(range(epochs), position=0):
         network = ising_efficient.BeliefNetwork(
             sparse_adj=adj_mat,
@@ -74,11 +75,11 @@ def main():
             external_field=external_field,
             init_state=ones_init,  # np.copy(random_init), #or 
             µ=µ,
-            beta=1.1,
+            beta=beta,
         )
         result = network.run_for_steps(n_steps, seed=i)
 
-        # coeffs = approx_global_coefficients_over_time(result, nbs, trials=20, interval=interval)
+        # coeffs = approx_global_coefficients_over_time(result, nbs, trials=trials, interval=interval)
         coeffs = np.mean(get_local_coefficients_over_time(result, nbs, interval=interval), axis=1)
         coeffs_over_time.append(coeffs)
         magnetization_erdos = np.mean(result, axis=1)
@@ -89,18 +90,23 @@ def main():
         axs[0].plot(mag, alpha=.4)
     axs[0].plot(np.mean(mags, axis=0), alpha=1, label="mean")
     axs[0].set_ylim(-1, 1)
-    axs[0].set_title("magnetization")
+    axs[0].set_ylabel("Magnetization")
+    axs[0].set_title(f"Magnetization in {plotname} point:\nβ = {beta}, λ = {l}")
+    axs[0].legend()
 
     for c in coeffs_over_time: 
-        axs[1].plot(x_ticks, c, alpha=.4)
+        axs[1].plot(x_ticks, c, alpha=.3)
     axs[1].plot(x_ticks, np.mean(coeffs_over_time, axis=0), alpha=1, label="mean")
-    axs[1].set_title("clustering coefficient")
+    axs[1].set_title("Average clustering coefficient\nTrials = 100, interval = 2")
     # axs[1].xlabel("Time steps")
-    # axs[1].ylabel("approx global clustering coefficient")
+    axs[1].set_ylabel("Clustering Coefficient")
     axs[1].grid(True, alpha=0.3)
     axs[1].legend()
 
-    plt.savefig('results/clust_coeff_small_world_long.png')
+    fig.suptitle("Erdos-Renyi topology magnetisation over clustering")
+    fig.supxlabel("Time steps")
+
+    plt.savefig(f'results/erdos_clust_coeff_{plotname}_smaller.png')
     plt.show()
 
 
